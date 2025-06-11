5def12d2bdbd483962b1050985ed32fb7b889cdc
ouare_lt2zch1 <ouare.ismo@outlook.com>
2025-06-11 04:49:10 -0400

    commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args)
+    {
+        Double doubleSpringCost = 350.00;
+        Double doubleSummerCost = 450.75;
+        Double doubleFallCost = 300.45;
+        Double doubleWinterCost = 555.00;
+        double doubleTotal = doubleSpringCost + doubleSummerCost + doubleFallCost + doubleWinterCost;
+        System.out.println("The yearly maintenance cost is : " + doubleTotal);
+
+
+
+
+
+    }
+    }
